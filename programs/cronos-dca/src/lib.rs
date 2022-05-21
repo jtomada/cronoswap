e6bf2688363dc40bbef06e2baf9bfe431f6f6084
allen0x1 <0xallenone@gmail.com>
2022-05-21 13:33:08 -0700

    added account init

--- programs/cronos-dca/src/lib.rs
+++ programs/cronos-dca/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +7,34 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod cronos_dca {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        amount: f64,
+        slippage: f64,
+    ) -> Result<()> {
+        ctx.accounts.dca_account.input_mint = ctx.accounts.input_mint.key();
+        ctx.accounts.dca_account.output_mint = ctx.accounts.output_mint.key();
+        ctx.accounts.dca_account.amount = amount;
+        ctx.accounts.dca_account.slippage = slippage;
+        msg!("initialized input mint, output mint, amount, slippage");
         Ok(())
     }
 }
 
+#[account]
+#[derive(Default)]
+pub struct DcaConfig {
+    input_mint: Pubkey,
+    output_mint: Pubkey,
+    amount: f64,
+    slippage: f64,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub dca_account: Account<'info, DcaConfig>,
+    pub input_mint: Account<'info, Mint>,
+    pub output_mint: Account<'info, Mint>,
+    pub owner: Signer<'info>
+}
